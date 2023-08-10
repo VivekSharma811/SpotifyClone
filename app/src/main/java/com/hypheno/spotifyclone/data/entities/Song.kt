e1242eaa4f64d7a4b85a273d28e6939c8db50036
Vivek <vivek.sharma@mpokket.com>
2023-08-10 12:42:15 +0530

    MusicDatabase added

---
+++ app/src/main/java/com/hypheno/spotifyclone/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.hypheno.spotifyclone.data.entities
+
+data class Song(
+    val title: String = "",
+    val subtitle: String = "",
+    val mediaId: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+)
---
+++ app/src/main/java/com/hypheno/spotifyclone/other/Constant.kt
@@ -0,0 +1,6 @@
+package com.hypheno.spotifyclone.other
+
+object Constant {
+
+    const val SONG_COLLECTION = "songs"
+}
