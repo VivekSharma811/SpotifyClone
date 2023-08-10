fd8bf0bb69676834d057bc9e05db93c39a1b1d2d
Vivek <vivek.sharma@mpokket.com>
2023-08-10 19:08:46 +0530

    MusicService implementation done

--- app/src/main/java/com/hypheno/spotifyclone/other/Constant.kt
+++ app/src/main/java/com/hypheno/spotifyclone/other/Constant.kt
@@ -3,4 +3,9 @@ package com.hypheno.spotifyclone.other
 object Constant {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
+    const val MEDIA_ROOT_ID = "root_id"
 }
